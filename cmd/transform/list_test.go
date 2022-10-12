1c27a770fd203280919ef22d98f58e1311c9899e
colin-mckibben-sp <coln.mckibben@sailpoint.com>
2022-10-12 15:42:05 -0400

    Added tests for transform command

---
+++ cmd/transform/list_test.go
@@ -0,0 +1,44 @@
+// Copyright (c) 2021, SailPoint Technologies, Inc. All rights reserved.
+
+package transform
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/sailpoint-oss/sailpoint-cli/mocks"
+)
+
+func TestNewListCmd(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := mocks.NewMockClient(ctrl)
+	client.EXPECT().
+		Get(gomock.Any(), gomock.Any()).
+		Return(&http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(bytes.NewReader([]byte("[]")))}, nil).
+		Times(1)
+
+	cmd := newListCmd(client)
+
+	b := new(bytes.Buffer)
+	cmd.SetOut(b)
+	cmd.PersistentFlags().StringP("transforms-endpoint", "e", transformsEndpoint, "Override transforms endpoint")
+
+	err := cmd.Execute()
+	if err != nil {
+		t.Fatalf("error execute cmd: %v", err)
+	}
+
+	out, err := io.ReadAll(b)
+	if err != nil {
+		t.Fatalf("error read out: %v", err)
+	}
+
+	if len(string(out)) == 0 {
+		t.Errorf("error empty out")
+	}
+}
---
+++ cmd/transform/update_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2021, SailPoint Technologies, Inc. All rights reserved.
+
+package transform
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/sailpoint-oss/sailpoint-cli/mocks"
+)
+
+func TestNewUpdateCmd(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := mocks.NewMockClient(ctrl)
+	client.EXPECT().
+		Put(gomock.Any(), gomock.Any(), "application/json", gomock.Any()).
+		Return(&http.Response{StatusCode: http.StatusOK, Body: io.NopCloser(bytes.NewReader([]byte("{}")))}, nil).
+		Times(1)
+
+	cmd := newUpdateCmd(client)
+
+	b := new(bytes.Buffer)
+	cmd.SetOut(b)
+	cmd.Flags().Set("file", "test_data/CreateTest.json")
+	cmd.PersistentFlags().StringP("transforms-endpoint", "e", transformsEndpoint, "Override transforms endpoint")
+
+	err := cmd.Execute()
+	if err != nil {
+		t.Fatalf("error execute cmd: %v", err)
+	}
+}
