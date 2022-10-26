a4d6e918e3f4073ce133e530b0319314e80c9cd1
colin-mckibben-sp <coln.mckibben@sailpoint.com>
2022-10-26 09:30:40 -0400

    Fixed root test command

--- cmd/root/root_test.go
+++ cmd/root/root_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Expected number of subcommands to `sp` root command
-const numRootSubcommands = 2
+const numRootSubcommands = 3
 
 func TestNewRootCmd_noArgs(t *testing.T) {
 	ctrl := gomock.NewController(t)
