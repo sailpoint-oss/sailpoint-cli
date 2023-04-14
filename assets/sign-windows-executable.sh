d08059342d8a95ced33d1b7ede8dad02e8ac2fcc
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-04-14 15:59:30 -0500

    Final Signing values, Commands validated prior

--- assets/sign-windows-executable.sh
+++ assets/sign-windows-executable.sh
@@ -13,14 +13,18 @@ if [ ! -f "$CERT_FILE" ]; then
   exit 1
 fi
 
-if [ -z "$CERT_PASSWORD" ]; then
-  echo "error Windows code-signing; no value for CERT_PASSWORD" >&2
+if [ -z "$KEY_FILE" ]; then
+  echo "skipping Windows code-signing; KEY_FILE not set" >&2
+  exit 0
+fi
+
+if [ ! -f "$KEY_FILE" ]; then
+  echo "error Windows code-signing; file '$KEY_FILE' not found" >&2
   exit 1
 fi
 
 osslsigncode sign -n "SailPoint CLI" -t http://timestamp.digicert.com \
-  -pkcs12 "$CERT_FILE" \
-  -pass "$CERT_PASSWORD" \
+  -certs "$CERT_FILE" -key "$KEY_FILE" \
   -in "$EXE" -out "$EXE"~
 
 mv "$EXE"~ "$EXE"
