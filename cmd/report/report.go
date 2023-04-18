472265874f97874f474aa59499d3bfe66d836e30
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-04-18 08:36:49 -0500

    Adjusted report command structure

--- cmd/report/report.go
+++ cmd/report/report.go
@@ -2,12 +2,24 @@
 package report
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
+	v3 "github.com/sailpoint-oss/golang-sdk/v3"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/config"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/templates"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/terminal"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/types"
 	"github.com/spf13/cobra"
 )
 
 func NewReportCommand() *cobra.Command {
+	var template string
+	var outputTypes []string
+	var folderPath string
 	cmd := &cobra.Command{
 		Use:     "report",
 		Short:   "Generate a report from a template using IdentityNow search queries",
@@ -15,14 +27,86 @@ func NewReportCommand() *cobra.Command {
 		Example: "sail report \"\"",
 		Aliases: []string{"rep"},
 		Args:    cobra.MaximumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			_, _ = fmt.Fprint(cmd.OutOrStdout(), cmd.UsageString())
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			err := config.InitConfig()
+			if err != nil {
+				return err
+			}
+
+			apiClient, err := config.InitAPIClient()
+			if err != nil {
+				return err
+			}
+
+			var selectedTemplate templates.ReportTemplate
+			reportTemplates, err := templates.GetReportTemplates()
+			if err != nil {
+				return err
+			}
+
+			if len(args) > 0 {
+				template = args[0]
+			} else {
+				template, err = templates.SelectTemplate(reportTemplates)
+				if err != nil {
+					return err
+				}
+			}
+			if template == "" {
+				return fmt.Errorf("no template specified")
+			}
+
+			config.Log.Info("Selected Template", "template", template)
+
+			matches := types.Filter(reportTemplates, func(st templates.ReportTemplate) bool { return st.Name == template })
+			if len(matches) < 1 {
+				return fmt.Errorf("no template matches for %s", template)
+			} else if len(matches) > 1 {
+				config.Log.Warn("multiple template matches, the first match will be used", "template", template)
+			}
+			selectedTemplate = matches[0]
+			varCount := len(selectedTemplate.Variables)
+			if varCount > 0 {
+				for i := 0; i < varCount; i++ {
+					varEntry := selectedTemplate.Variables[i]
+					resp := terminal.InputPrompt("Input " + varEntry.Prompt + ":")
+					selectedTemplate.Raw = []byte(strings.ReplaceAll(string(selectedTemplate.Raw), "{{"+varEntry.Name+"}}", resp))
+				}
+				err := json.Unmarshal(selectedTemplate.Raw, &selectedTemplate.Queries)
+				if err != nil {
+					return err
+				}
+			}
+
+			for i := 0; i < len(selectedTemplate.Queries); i++ {
+
+				currentQuery := selectedTemplate.Queries[i]
+
+				searchQuery := v3.NewSearch()
+				query := v3.NewQuery()
+				query.SetQuery(currentQuery.QueryString)
+				searchQuery.Query = query
+
+				resp, err := apiClient.V3.SearchApi.SearchCount(context.TODO()).Search(*searchQuery).Execute()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+					fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", resp)
+				}
+				selectedTemplate.Queries[i].ResultCount = resp.Header["X-Total-Count"][0]
+			}
+
+			for i := 0; i < len(selectedTemplate.Queries); i++ {
+				currentQuery := selectedTemplate.Queries[i]
+				fmt.Println(currentQuery.QueryTitle + ": " + currentQuery.ResultCount)
+			}
+
+			return nil
 		},
 	}
 
-	cmd.AddCommand(
-		newTemplateCmd(),
-	)
+	cmd.Flags().StringArrayVarP(&outputTypes, "output", "o", []string{"json"}, "the sort value for the api call (examples)")
+	cmd.Flags().StringVarP(&folderPath, "folderPath", "f", "reports", "folder path to save the reports in. If the directory doesn't exist, then it will be automatically created. (default is the current working directory)")
 
 	return cmd
 
