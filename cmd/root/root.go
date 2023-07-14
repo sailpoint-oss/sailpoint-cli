84db6f9e5f466c1cfb05a7d9a348e1390e313435
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-07-14 09:08:27 -0500

    Added ENV flag support

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -4,7 +4,6 @@ package root
 import (
 	"fmt"
 
-	"github.com/sailpoint-oss/sailpoint-cli/cmd/configure"
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/connector"
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/environment"
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/report"
@@ -14,6 +13,7 @@ import (
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/spconfig"
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/transform"
 	"github.com/sailpoint-oss/sailpoint-cli/cmd/va"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/config"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/terminal"
 	"github.com/spf13/cobra"
 )
@@ -21,6 +21,7 @@ import (
 var version = "1.2.0"
 
 func NewRootCmd() *cobra.Command {
+	var env string
 	root := &cobra.Command{
 		Use:          "sail",
 		Short:        "The SailPoint CLI allows you to administer your IdentityNow tenant from the command line.\nNavigate to https://developer.sailpoint.com/idn/tools/cli to learn more.",
@@ -32,16 +33,25 @@ func NewRootCmd() *cobra.Command {
 			DisableDescriptions: true,
 		},
 		Run: func(cmd *cobra.Command, args []string) {
+			var tempEnv string
+			if env != "" {
+				tempEnv = config.GetActiveEnvironment()
+				config.SetActiveEnvironment(env)
+			}
+
 			_, _ = fmt.Fprint(cmd.OutOrStdout(), cmd.UsageString())
+
+			if tempEnv != "" {
+				config.SetActiveEnvironment(tempEnv)
+			}
 		},
 	}
 
 	t := &terminal.Term{}
 
 	root.AddCommand(
-		set.NewSetCmd(),
+		set.NewSetCmd(t),
 		environment.NewEnvironmentCmd(),
-		configure.NewConfigureCmd(t),
 		connector.NewConnCmd(t),
 		transform.NewTransformCmd(),
 		va.NewVACmd(t),
@@ -50,5 +60,8 @@ func NewRootCmd() *cobra.Command {
 		report.NewReportCmd(),
 		sdk.NewSDKCmd(),
 	)
+
+	root.PersistentFlags().StringVar(&env, "env", "", "Environment to use for SailPoint CLI commands")
+
 	return root
 }
