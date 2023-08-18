1c853d52442ea9e57627ecbf8215f8dece67daaf
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2023-08-18 09:22:52 -0500

    finalized va list command

--- cmd/va/list.go
+++ cmd/va/list.go
@@ -7,6 +7,7 @@ import (
 	sailpoint "github.com/sailpoint-oss/golang-sdk"
 	"github.com/sailpoint-oss/golang-sdk/beta"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/config"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/output"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/sdk"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/util"
 	"github.com/spf13/cobra"
@@ -35,15 +36,22 @@ func newListCommand() *cobra.Command {
 				return sdk.HandleSDKError(resp, clustersErr)
 			}
 
+			var clients [][]string
+
 			for _, cluster := range clusters {
 				for _, id := range cluster.ClientIds {
 					clientStatus, resp, clientErr := apiClient.Beta.ManagedClientsApi.GetManagedClientStatus(context.TODO(), id).Type_("VA").Execute()
 					if clientErr != nil {
 						return sdk.HandleSDKError(resp, clientErr)
 					}
-					cmd.Println(util.PrettyPrint(clientStatus))
+
+					if clientStatus.Status != "NOT_CONFIGURED" {
+						clients = append(clients, []string{*cluster.Name, clientStatus.Body["internal_ip"].(string), clientStatus.Body["id"].(string)})
+					}
 				}
 			}
+
+			output.WriteTable(cmd.OutOrStdout(), []string{"Cluster", "IP Address", "ID"}, clients)
 
 			return nil
 		},
