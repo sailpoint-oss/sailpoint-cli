a6dcbbe7d1ec53c9ca23e77d4131e636c984585e
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-03-05 12:18:41 -0500

    update go to 1.21 and update root_test.go to fix sub command count test

--- cmd/root/root_test.go
+++ cmd/root/root_test.go
@@ -13,7 +13,7 @@ import (
 
 // Expected number of subcommands to `sail` root command
 const (
-	numRootSubcommands = 12
+	numRootSubcommands = 13
 )
 
 func TestNewRootCmd_noArgs(t *testing.T) {
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/sailpoint-oss/sailpoint-cli
 
-go 1.19
+go 1.21
 
 require (
 	github.com/charmbracelet/bubbles v0.16.1
