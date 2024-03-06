37919b6bdcd647c2bc2e9c628dfb7f3821b01d8c
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-03-06 13:19:13 -0500

    added switch enviorment logic

--- cmd/environment/environment.go
+++ cmd/environment/environment.go
@@ -4,6 +4,8 @@ package environment
 import (
 	_ "embed"
 
+	"github.com/charmbracelet/log"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/config"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/util"
 	"github.com/spf13/cobra"
 )
@@ -13,7 +15,7 @@ var environmentHelp string
 
 func NewEnvironmentCommand() *cobra.Command {
 	help := util.ParseHelp(environmentHelp)
-
+	var env string
 	cmd := &cobra.Command{
 		Use:     "environment",
 		Short:   "Manage Environments for the CLI",
@@ -21,7 +23,23 @@ func NewEnvironmentCommand() *cobra.Command {
 		Example: help.Example,
 		Aliases: []string{"env"},
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
+			environments := config.GetEnvironments()
+
+			if len(args) > 0 {
+				env = args[0]
+			} else {
+				env = config.GetActiveEnvironment()
+			}
+
+			if environments[env] != nil {
+				config.SetActiveEnvironment(env)
+				log.Info("Active Environment", "env", env)
+			} else if env != "help" {
+				log.Warn("Environment does not exist", "env", env)
+			} else {
+				cmd.Help()
+			}
+
 		},
 	}
 
