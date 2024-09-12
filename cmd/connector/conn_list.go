8fc214747e8e36f460ea3b5c39105ac35183684e
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-09-12 10:30:53 -0400

    update sail conn list command to show tags and versions and sort the table by Alias name

--- cmd/connector/conn_list.go
+++ cmd/connector/conn_list.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sort"
 
 	"github.com/olekukonko/tablewriter"
 	"github.com/sailpoint-oss/sailpoint-cli/internal/client"
+	"github.com/sailpoint-oss/sailpoint-cli/internal/util"
 	"github.com/spf13/cobra"
 )
 
@@ -40,19 +42,74 @@ func newConnListCmd(client client.Client) *cobra.Command {
 				return err
 			}
 
-			var conns []connector
+			var conns []connectorList
 			err = json.Unmarshal(raw, &conns)
 			if err != nil {
 				return err
 			}
 
-			table := tablewriter.NewWriter(cmd.OutOrStdout())
-			table.SetHeader(connectorColumns)
-			for _, v := range conns {
-				table.Append(v.columns())
-			}
-			table.Render()
+			// Sort connectors by Alias
+			sort.Slice(conns, func(i, j int) bool {
+				return conns[i].Alias < conns[j].Alias
+			})
 
+			table := tablewriter.NewWriter(cmd.OutOrStdout())
+			table.SetHeader(connectorListColumns)
+
+			// Process each connector and populate the table
+			for _, conn := range conns {
+				connectorRef := conn.ID
+				if connectorRef == "" {
+					continue
+				}
+
+				// Build the tags endpoint using the connectorRef
+				tagsEndpoint := util.ResourceUrl(endpoint, connectorRef, "tags")
+				tagsResp, err := client.Get(cmd.Context(), tagsEndpoint)
+				if err != nil {
+					return err
+				}
+				defer tagsResp.Body.Close()
+
+				if tagsResp.StatusCode != http.StatusOK {
+					body, _ := io.ReadAll(tagsResp.Body)
+					return fmt.Errorf("non-200 response: %s\nbody: %s", tagsResp.Status, body)
+				}
+
+				// Process the response for the tags request
+				tagsRaw, err := io.ReadAll(tagsResp.Body)
+				if err != nil {
+					return err
+				}
+
+				var tags []tag
+				err = json.Unmarshal(tagsRaw, &tags)
+				if err != nil {
+					return err
+				}
+
+				// Prepare data for the table
+				var tagNames []string
+				var versions []string
+				for _, t := range tags {
+					tagNames = append(tagNames, t.TagName)
+					versions = append(versions, fmt.Sprintf("%d", t.ActiveVersion))
+				}
+				tagsString := fmt.Sprintf("%s", tagNames)
+				versionsString := fmt.Sprintf("%s", versions)
+
+				// Add the row to the table
+				row := []string{
+					conn.ID,
+					conn.Alias,
+					tagsString,
+					versionsString,
+				}
+				table.Append(row)
+			}
+
+			// Render the table
+			table.Render()
 			return nil
 		},
 	}
