46b2edb9ef9d8681dce224c155d10c3039ee7e1e
darrell-thobe-sp <darrell.thobe@sailpoint.com>
2024-09-12 12:48:59 -0400

    revert spec.ts change

--- cmd/connector/static/connector/src/index.spec.ts
+++ cmd/connector/static/connector/src/index.spec.ts
@@ -1,34 +1,24 @@
-import { connector } from './index';
-import { Connector, RawResponse, ResponseType, StandardCommand, Context } from '@sailpoint/connector-sdk';
-import { PassThrough } from 'stream';
+import { connector } from './index'
+import { Connector, RawResponse, ResponseType, StandardCommand } from '@sailpoint/connector-sdk'
+import { PassThrough } from 'stream'
 
-// Mock configuration
 const mockConfig: any = {
     token: 'xxx123'
-};
-process.env.CONNECTOR_CONFIG = Buffer.from(JSON.stringify(mockConfig)).toString('base64');
-
-// Mock implementation of Context
-const mockContext: Context = {
-    reloadConfig: async () => {
-        // Mock implementation
-        return Promise.resolve();
-    },
-    // Add other properties and methods if required
-};
+}
+process.env.CONNECTOR_CONFIG = Buffer.from(JSON.stringify(mockConfig)).toString('base64')
 
 describe('connector unit tests', () => {
 
     it('connector SDK major version should be the same as Connector.SDK_VERSION', async () => {
-        expect((await connector()).sdkVersion).toStrictEqual(Connector.SDK_VERSION);
-    });
+        expect((await connector()).sdkVersion).toStrictEqual(Connector.SDK_VERSION)
+    })
 
     it('should execute stdTestConnectionHandler', async () => {
         await (await connector())._exec(
             StandardCommand.StdTestConnection,
-            mockContext, // Pass the mockContext here
+            {},
             undefined,
-            new PassThrough({ objectMode: true }).on('data', (chunk) => expect(chunk).toStrictEqual(new RawResponse({}, ResponseType.Output)))
-        );
-    });
-});
+            new PassThrough({ objectMode: true }).on('data', (chunk) => expect(chunk).toStrictEqual(new RawResponse ({}, ResponseType.Output)))
+        )
+    })
+})
