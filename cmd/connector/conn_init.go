fa9f1d251c0e1b93e88881c91038d5d618d3a5a1
Tyler Mairose <tyler.mairose@sailpoint.com>
2025-03-03 12:51:27 -0500

    Add flag for sail conn init to pull from available colab connectors

--- cmd/connector/conn_init.go
+++ cmd/connector/conn_init.go
@@ -5,17 +5,67 @@ package connector
 import (
 	"bytes"
 	"embed"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/fs"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 
+	"github.com/go-git/go-git/v5"
+
+	"github.com/sailpoint-oss/sailpoint-cli/internal/tui"
 	"github.com/spf13/cobra"
 )
+
+const githubAPI = "https://api.github.com/search/repositories"
+
+type Repo struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	HTMLURL     string `json:"html_url"`
+	CloneURL    string `json:"clone_url"`
+}
+
+type SearchResult struct {
+	Items []Repo `json:"items"`
+}
+
+func fetchRepositories(org string, query string) ([]Repo, error) {
+	searchQuery := fmt.Sprintf("org:%s %s", org, query)
+	params := url.Values{}
+	params.Add("q", searchQuery)
+	apiURL := fmt.Sprintf("%s?%s", githubAPI, params.Encode())
+
+	req, err := http.NewRequest("GET", apiURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("GitHub API request failed with status: %s", resp.Status)
+	}
+
+	var result SearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, err
+	}
+
+	return result.Items, nil
+}
 
 //go:embed static/connector/*
 var connectorStaticDir embed.FS
@@ -31,6 +81,8 @@ const (
 // It accepts one argument, project name, and generates appropriate directories and files
 // to set up a working, sample project.
 func newConnInitCommand() *cobra.Command {
+	var colab bool
+	var repoUrl string
 	cmd := &cobra.Command{
 		Use:     "init <connector-name>",
 		Short:   "Initialize new connector project",
@@ -55,50 +107,82 @@ func newConnInitCommand() *cobra.Command {
 				return
 			}
 
-			err := fs.WalkDir(connectorStaticDir, connectorTemplatePath, func(path string, d fs.DirEntry, err error) error {
+			if colab {
+				repos, err := fetchRepositories("sailpoint-oss", "colab-saas-conn")
 				if err != nil {
-					return err
+					fmt.Println("Error fetching repositories:", err)
+					return
 				}
 
-				if d.Name() == connectorDirName {
-					return nil
+				if len(repos) == 0 {
+					fmt.Println("No repositories found.")
+					return
 				}
 
-				if d.IsDir() {
-					if err := createDir(filepath.Join(projName, d.Name())); err != nil {
-						return err
-					}
+				sort.Slice(repos, func(i, j int) bool {
+					return repos[i].Name < repos[j].Name
+				})
+
+				repoUrl, err = SelectColabRepo(repos)
+
+				if err != nil {
+					fmt.Println("Error selecting repository:", err)
+					return
+				}
+
+				fmt.Println("Selected repository:", repoUrl)
+				if err := cloneRepo(repoUrl, projName); err != nil {
+					fmt.Println("Error cloning repository:", err)
 				} else {
-					fileName := filepath.Join(projName, strings.TrimPrefix(path, connectorTemplatePath))
-
-					data, err := connectorStaticDir.ReadFile(path)
-					if err != nil {
-						return err
-					}
-
-					if err := createFile(fileName, data); err != nil {
-						return err
-					}
+					fmt.Println("Repository cloned successfully!")
 				}
 
-				if d.Name() == packageJsonName || d.Name() == connectorSpecName {
-					fileAbsPath, err := filepath.Abs(filepath.Join(projName, strings.TrimPrefix(path, connectorTemplatePath)))
+			} else {
+				err := fs.WalkDir(connectorStaticDir, connectorTemplatePath, func(path string, d fs.DirEntry, err error) error {
 					if err != nil {
 						return err
 					}
 
-					if err := createFileFromTemplate(projName, d.Name(), fileAbsPath); err != nil {
-						return err
+					if d.Name() == connectorDirName {
+						return nil
 					}
+
+					if d.IsDir() {
+						if err := createDir(filepath.Join(projName, d.Name())); err != nil {
+							return err
+						}
+					} else {
+						fileName := filepath.Join(projName, strings.TrimPrefix(path, connectorTemplatePath))
+
+						data, err := connectorStaticDir.ReadFile(path)
+						if err != nil {
+							return err
+						}
+
+						if err := createFile(fileName, data); err != nil {
+							return err
+						}
+					}
+
+					if d.Name() == packageJsonName || d.Name() == connectorSpecName {
+						fileAbsPath, err := filepath.Abs(filepath.Join(projName, strings.TrimPrefix(path, connectorTemplatePath)))
+						if err != nil {
+							return err
+						}
+
+						if err := createFileFromTemplate(projName, d.Name(), fileAbsPath); err != nil {
+							return err
+						}
+						return nil
+					}
+
 					return nil
+				})
+				if err != nil {
+					_ = os.RemoveAll(projName)
+					printError(cmd.ErrOrStderr(), err)
+					return
 				}
-
-				return nil
-			})
-			if err != nil {
-				_ = os.RemoveAll(projName)
-				printError(cmd.ErrOrStderr(), err)
-				return
 			}
 
 			printDir(cmd.OutOrStdout(), projName, 0)
@@ -106,7 +190,32 @@ func newConnInitCommand() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().BoolVarP(&colab, "from-colab", "c", false, "Initialize a new connector project from a Colab connector")
+
 	return cmd
+}
+
+func SelectColabRepo[T Repo](repos []Repo) (string, error) {
+	var prompts []tui.Choice
+	for i := 0; i < len(repos); i++ {
+		temp := repos[i]
+
+		prompts = append(prompts, tui.Choice{Title: temp.Name, Description: temp.Description, Id: temp.CloneURL})
+	}
+
+	intermediate, err := tui.PromptList(prompts, "Select a Colab connector to initialize")
+	if err != nil {
+		return "", err
+	}
+	return intermediate.Id, nil
+
+}
+
+func cloneRepo(repoURL, destination string) error {
+	_, err := git.PlainClone(destination, false, &git.CloneOptions{
+		URL: repoURL,
+	})
+	return err
 }
 
 // createFileFromTemplate fills the template file withe parameters,
--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/sailpoint-oss/sailpoint-cli
 
-go 1.21
+go 1.23.0
+
+toolchain go1.23.6
 
 require (
 	github.com/bhmj/jsonslice v1.1.3
@@ -10,6 +12,7 @@ require (
 	github.com/charmbracelet/lipgloss v0.8.0
 	github.com/charmbracelet/log v0.2.4
 	github.com/fatih/color v1.15.0
+	github.com/go-git/go-git/v5 v5.14.0
 	github.com/golang/mock v1.6.0
 	github.com/kr/pretty v0.3.1
 	github.com/logrusorgru/aurora v2.0.3+incompatible
@@ -22,12 +25,13 @@ require (
 	github.com/spf13/cobra v1.7.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.18.1
+	github.com/tidwall/pretty v1.2.1
 	github.com/vbauerster/mpb/v8 v8.6.1
 	github.com/zalando/go-keyring v0.2.3
-	golang.org/x/crypto v0.21.0
-	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
+	golang.org/x/crypto v0.35.0
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
 	golang.org/x/oauth2 v0.15.0
-	golang.org/x/term v0.18.0
+	golang.org/x/term v0.29.0
 	gopkg.in/alessio/shellescape.v1 v1.0.0-20170105083845-52074bc9df61
 	gopkg.in/square/go-jose.v2 v2.6.0
 	gopkg.in/yaml.v2 v2.4.0
@@ -39,6 +43,9 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.1.5 // indirect
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/alecthomas/chroma v0.10.0 // indirect
@@ -47,16 +54,24 @@ require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/bhmj/xpression v0.9.1 // indirect
+	github.com/cloudflare/circl v1.6.0 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
 	github.com/danieljoos/wincred v1.2.0 // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -71,26 +86,30 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/pjbgf/sha1cd v0.3.2 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
-	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
 	github.com/spf13/cast v1.6.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	github.com/yuin/goldmark v1.5.6 // indirect
 	github.com/yuin/goldmark-emoji v1.0.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/validator.v2 v2.0.1 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
