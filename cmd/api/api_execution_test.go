db54bf043b37f7b2ca5e3be5a94d9dbe6c735c14
Luke Hagar <lukeslakemail@gmail.com>
2025-03-27 09:52:53 -0500

    adding test

--- cmd/api/api_execution_test.go
+++ cmd/api/api_execution_test.go
@@ -18,3 +18,45 @@ func TestTenantEndpoint(t *testing.T) {
 		t.Errorf("Failed to execute command: %v", err)
 	}
 }
+
+func TestListTransformations(t *testing.T) {
+	// Create the GET command
+	cmd := newGetCmd()
+
+	// Set the endpoint
+	cmd.SetArgs([]string{"/v2024/transforms"})
+
+	cmd.Flags().Set("pretty", "true")
+	cmd.Flags().Set("query", "limit=2")
+
+	// Capture stdout
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	defer func() { os.Stdout = oldStdout }()
+
+	err = cmd.Execute()
+	if err != nil {
+		t.Fatalf("TestNewCreateCmd: Unable to execute the command successfully: %v", err)
+	}
+
+	// Close the writer and read the output
+	w.Close()
+	responseBytes, err := io.ReadAll(r)
+	if err != nil {
+		t.Fatalf("Error reading stdout: %v", err)
+	}
+
+	// Count the number of transformations in the response
+	transformations := strings.Count(string(responseBytes), "id")
+
+	// Check if the response is as expected
+	if transformations != 2 {
+		t.Errorf("Expected 2 transformations in the response, but got: %d", transformations)
+	}
+}
+
+
+
+
+}
