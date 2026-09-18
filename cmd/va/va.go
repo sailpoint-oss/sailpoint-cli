823f6e6eab0cbcbb155138e5051d7d750b4058b8
Tyler Mairose <tyler.mairose@sailpoint.com>
2026-09-18 10:19:25 -0400

    Remove the un-used troubleshoot command

--- cmd/va/va.go
+++ cmd/va/va.go
@@ -26,7 +26,6 @@ func NewVACommand(term terminal.Terminal) *cobra.Command {
 
 	cmd.AddCommand(
 		newCollectCommand(term),
-		// newTroubleshootCommand(),
 		newGetCommand(),
 		newParseCommand(),
 		newUpdateCommand(term),
